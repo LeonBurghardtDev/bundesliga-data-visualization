61c490836b7d0d50507dc4de37d00c0149b53241
tr3x <leon@tr3x.tech>
2022-12-25 03:45:29 +0100

    removed meta tag, added doc.title = title

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState} from 'react';
 import Table from './Table';
 import './app.css';
 import GoalScorer from './GoalScorer';
@@ -7,33 +7,17 @@ import Matchday from './Matchday.js';
 import axios from 'axios';
 
 function App() {
-    const [tableData, setTableData] = useState([]);
-    const [matchdayData, setMatchdayData] = useState([]);
     let currentYear = new Date().getFullYear();
     if(new Date().getMonth() < 7) {
         currentYear--;
     }
     const [selectedSeason, setSelectedSeason] = useState(currentYear);
-    const [selectedMatchday, setSelectedMatchday] = useState(1);
 
-
-    useEffect(() => {
-
-        async function fetchData() {
-            const response = await axios.get(`https://www.openligadb.de/api/getbltable/bl1/${selectedSeason}`);
-            setTableData(response.data);
-        }
-        fetchData();
-
-    }, []);
-
+    document.title = 'Bundesliga Data';
 
 
     return (
         <>
-            <meta>
-                <title>Bundesliga Data</title>
-            </meta>
             <header>
                 <div style={{ display: 'block', textAlign:'center' }}>
                     DFL Bundesliga Data  visualization by <a href="https://tr3x.xyz" style={{textDecoration:'none',color:"white"}}> @LeonBurghardt</a> | Database provided by <a href="https://www.openligadb.de" style={{textDecoration:'none',color:"white"}}>@OpenLigaDB</a>
@@ -46,17 +30,12 @@ function App() {
                     <Table
                         onSeasonChange={(season, data) => {
                             setSelectedSeason(season);
-                            setTableData(data);
                         }}
                     />
                 </div>
                 <div style={{ flexGrow: 2 }}>
                     <Matchday
                         selectedSeason={selectedSeason}
-                        onMatchdayChange={(matchday, data) => {
-                            setSelectedMatchday(matchday);
-                            setMatchdayData(data);
-                        }}
                     />
                 </div>
                 <div style={{ flexGrow: 3 }}>
